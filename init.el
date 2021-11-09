dfe9823d10d177129dd576edd84fe8cb82604200
Daniel M German <dmg@turingmachine.org>
2021-11-09 12:38:28 -0800

    minor updates cleaning up issues

--- init.el
+++ init.el
@@ -6,25 +6,35 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(org-bullets use-package))
+ '(package-selected-packages
+   '(good-scroll smooth-scroll yasnippet org-bullets use-package))
  '(visible-bell t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(bold ((t (:foreground "red" :weight bold))))
+ '(code ((t (:foreground "red" :weight bold))))
  '(code-block ((t (:background "whitesmoke" :extend t))))
  '(hl-line ((t (:background "darkseagreen1"))))
+ '(italic ((t (:foreground "red" :weight bold))))
  '(org-block ((t (:inherit fixed-pitch :height 1.0 :background "#FFFFE2" :extend t))))
  '(org-block-background ((t (:background "lightgray" :extend t))))
  '(org-block-begin-line ((t (:extend t :foreground "darkgrey"))))
  '(org-block-end-line ((t (:extend t :foreground "darkgrey"))))
  '(org-code ((t (:inherit (shadow fixed-pitch)))))
+ '(org-column-title ((t (:foreground "darkgreen" :weight bold))))
  '(org-document-title ((t (:height 1.0 :underline nil))))
+ '(org-example ((t (:foreground "darkgreen" :weight bold))))
  '(org-level-1 ((t (:inherit default :weight bold :height 1.5))))
  '(org-level-2 ((t (:inherit default :weight bold :height 1.3))))
  '(org-level-3 ((t (:inherit default :weight bold :height 1.15))))
- '(org-level-4 ((t (:inherit default :weight bold :height 1.1)))))
+ '(org-level-4 ((t (:inherit default :weight bold :height 1.1))))
+ '(org-table-header ((t (:foreground "darkgreen" :weight bold))))
+ '(org-verbatim ((t (:foreground "darkblue" :weight bold))))
+ '(underline ((t (:foreground "orange" :weight bold))))
+ '(verbatim ((t (:foreground "darkblue" :weight bold)))))
 
 (require 'package)
 (add-to-list 'package-archives
